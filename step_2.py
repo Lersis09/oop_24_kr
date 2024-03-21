85216014c6e36cac7e4e7eccd7e2349af0c47ac1
Leriya <133794400+Lersis09@users.noreply.github.com>
2024-03-21 19:59:51 +0200

    Update step_2.py

--- step_2.py
+++ step_2.py
@@ -1,16 +1,11 @@
 # підключення необхідних бібліотек
 import cv2
 
-sources = {'video1': "C:/Users/User/PycharmProjects/oop/video/bunnies.mp4"}
-colorspaces = {'Gray': cv2.COLOR_BGR2GRAY, 'XYZ': cv2.COLOR_BGR2XYZ, 'LAB': cv2.COLOR_BGR2LAB,
-               'YUV': cv2.COLOR_BGR2YUV, 'HSV': cv2.COLOR_BGR2HSV}
-
-
-def colorspace_change(input_frame, colorspace_index):
-    return cv2.cvtColor(input_frame, colorspaces.get(colorspace_index))
+sources = {'video1': "C:/Users/User/PycharmProjects/oop/video/bunnies.mp4", 'video2': "web"}
 
 
 def main():
+    # метод зчитування даних з відеофайлу (стор. 142 - 145)
     cap = cv2.VideoCapture(sources.get('video1'))
     # Перевірка готовності веб-камери
     while cap.isOpened():
@@ -20,27 +15,32 @@ def main():
         if not ret:
             print("Помилка запису фрейму!")
             break
-
-        # Зміна колірного простору зображення (фрейму)
-        frame_gray = colorspace_change(frame, 'Gray')
-        frame_XYZ = colorspace_change(frame, 'XYZ')
-        frame_LAB = colorspace_change(frame, 'LAB')
-        frame_YUV = colorspace_change(frame, 'YUV')
-        frame_HSV = colorspace_change(frame, 'HSV')
-
         # Відображення результату
         cv2.imshow('frame', frame)
-        cv2.imshow('frame Grayscale', frame_gray)
-        cv2.imshow('frame XYZ', frame_XYZ)
-        cv2.imshow('frame LAB', frame_LAB)
-        cv2.imshow('frame YUV', frame_YUV)
-        cv2.imshow('frame HSV', frame_HSV)
         if cv2.waitKey(25) == ord('q'):
             break
     # Завершуємо запис у кінці роботи
     cap.release()
     cv2.destroyAllWindows()
-
+ # зчитування відно з веб-камери
+    cap = cv2.VideoCapture(0)
+    # перевірка готовності веб-камери
+    while cap.isOpened():
+        # Запис фреймів
+        ret, frame = cap.read()
+        # при виникненні помилки запису
+        if not ret:
+            print("Помилка запису файлу")
+            break
+        # обробка поточного фрейму
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # відображення результату
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    # Завершуємо запис у кінці роботи
+    cap.release()
+    cv2.destroyAllWindows()
 
 # при запуску як головного файлу
 if __name__ == '__main__':
