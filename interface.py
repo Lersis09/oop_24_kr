8a833795a13eaba2b34c41e1efd12cfd6eaa8cce
Leriya <133794400+Lersis09@users.noreply.github.com>
2024-05-27 11:12:00 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,66 +1,117 @@
 import cv2
 import numpy as np
 import tkinter as tk
+from tkinter import filedialog
 from PIL import Image, ImageTk
 
-class TaskWindow(tk.Frame):
-    """Класс MainWindow, наследующий Frame"""
 
-    def __init__(self, parent):
-        """Настройка графического интерфейса"""
-        super().__init__(parent)
-        # Растянуть фрейм
-        self.pack(fill=tk.BOTH, expand=1)
-        # Растянуть сетку
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        # Создание виджетов (4 командные кнопки)
-        self.but1 = tk.Button(self, text="Геометричне перетворення(дзеркальне відображення)", command=self.show_video1)
-        self.but2 = tk.Button(self, text="Операція над зображенням(детекція кутів Ши-Томасі)", command=self.show_video2)
-        self.but3 = tk.Button(self, text="Кольорове перетворення(YUV)", command=self.show_video3)
-        self.but4 = tk.Button(self, text="Фільтрація зображення(Гаусів фільтр)", command=self.show_video4)
-        # Создание метки для отображения видео
-        self.label_video = tk.Label(self)
-        self.label_video.grid(row=0, column=0, sticky="nsew")
-        # Размещение виджетов в сетке основного окна
-        self.but1.grid(row=1, column=0, sticky="nsew")
-        self.but2.grid(row=1, column=1, sticky="nsew")
-        self.but3.grid(row=1, column=2, sticky="nsew")
-        self.but4.grid(row=1, column=3, sticky="nsew")
+class VideoApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-    def show_video1(self):
-        self.show_video_from_file("step_3.py")
+        self.title("Застосування фільтрів")
 
-    def show_video2(self):
-        self.show_video_from_file("step_6.py")
+        self.video_source = None
+        self.cap = None
+        self.video_width = 640
+        self.video_height = 480
 
-    def show_video3(self):
-        self.show_video_from_file("step_4.py")
+        self.create_widgets()
 
-    def show_video4(self):
-        self.show_video_from_file("step_5.py")
+    def create_widgets(self):
+        self.video_label = tk.Label(self)
+        self.video_label.pack()
 
-    def show_video_from_file(self, filename):
-        cap = cv2.VideoCapture(filename)
-        if not cap.isOpened():
-            print(f"Помилка: не вдається відкрити файл {filename}")
-            return
+        self.open_file_button = tk.Button(self, text="Вибрати файл", command=self.open_file)
+        self.open_file_button.pack()
 
-        while cap.isOpened():
-            ret, frame = cap.read()
+        self.filter_buttons = []
+        filters = ["Віддзеркалення", "Ши-Томасі", "Кольорове перетворення", "Гаусів фільтр"]
+        for filter_name in filters:
+            button = tk.Button(self, text=filter_name, command=lambda f=filter_name: self.apply_filter(f))
+            button.pack()
+            self.filter_buttons.append(button)
+
+        self.open_camera_button = tk.Button(self, text="Увімкнути веб-камеру", command=self.open_camera)
+        self.open_camera_button.pack()
+
+        self.stop_button = tk.Button(self, text="Стоп", command=self.stop_video)
+        self.stop_button.pack()
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi")])
+        if file_path:
+            self.video_source = file_path
+            self.cap = cv2.VideoCapture(file_path)
+            self.play_video()
+
+    def open_camera(self):
+        self.video_source = 0
+        self.cap = cv2.VideoCapture(self.video_source)
+        self.play_video()
+
+    def play_video(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.resize(frame, (self.video_width, self.video_height))
+            img = Image.fromarray(frame)
+            img = ImageTk.PhotoImage(image=img)
+            self.video_label.img = img
+            self.video_label.config(image=img)
+            self.video_label.after(10, self.play_video)
+        else:
+            self.stop_video()
+
+    def stop_video(self):
+        if self.cap:
+            self.cap.release()
+            self.video_source = None
+            cv2.destroyAllWindows()
+
+    def apply_filter(self, filter_name):
+        if self.cap:
+            ret, frame = self.cap.read()
             if ret:
+                if filter_name == "Віддзеркалення":
+                    frame = self.apply_mirror(frame)
+                elif filter_name == "Ши-Томасі":
+                    frame = self.apply_corner_detection(frame,max_corners=20, quality_level=0.01, min_dist=50)
+                elif filter_name == "Кольорове перетворення":
+                    frame = self.apply_colorspace_change(frame,'YUV')
+                elif filter_name == "Гаусів фільтр":
+                    frame = self.apply_gaussian_filter(frame, kernel_size=11)
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 img = Image.fromarray(frame)
                 img = ImageTk.PhotoImage(image=img)
-                self.label_video.img = img
-                self.label_video.config(image=img)
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    break
-            else:
-                print(f"Помилка: не вдається прочитати кадр з файлу {filename}")
-                break
+                self.video_label.img = img
+                self.video_label.config(image=img)
 
-        cap.release()
-        cv2.destroyAllWindows()
+    def apply_mirror(self, frame):
+        rows, cols = frame.shape[:2]
+        src_points = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1]])
+        dst_points = np.float32([[cols - 1, 0], [0, 0], [cols - 1, rows - 1]])
+        affine_matrix = cv2.getAffineTransform(src_points, dst_points)
+        img_output = cv2.warpAffine(frame, affine_matrix, (cols, rows))
+        return img_output
+
+    def apply_corner_detection(self, frame, max_corners=5, quality_level=0.01, min_dist=20):
+        new_frame = frame.copy()
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        corners = cv2.goodFeaturesToTrack(gray_frame, max_corners, quality_level, min_dist)
+        corners = np.int0(corners)
+        for item in corners:
+            x, y = item.ravel()
+            cv2.circle(new_frame, (x, y), 5, (255, 255, 255), -1)
+        return new_frame
+
+    def apply_colorspace_change(self, frame, colorspace_index=cv2.COLOR_BGR2GRAY):
+        return cv2.cvtColor(frame, colorspace_index)
+
+    def apply_gaussian_filter(self, frame, kernel_size=3):
+        return cv2.GaussianBlur(frame, (kernel_size, kernel_size), 0)
 
 
+if __name__ == "__main__":
+    app = VideoApp()
+    app.mainloop()
